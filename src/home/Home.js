a4f60f978a09534db6a98dbb747e1a85a1f336d2
Pankaj Nema <pankaj@routier.io>
2020-02-25 18:05:11 +0530

    initial commit

---
+++ src/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+     <div className="home">
+         This is home page.
+     </div>
+    );
+  }
+}
+
+export default Home;
